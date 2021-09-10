9a22a4b28b1b2dba4f5b0bc1ca04ce5eeae2305e
Wildrimak <finalwildrimak@gmail.com>
2021-09-10 17:11:38 -0300

    Configuração oauth2 password flow

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+insert into cliente(id, nome) values (1, 'maria');
+insert into cliente(id, nome) values (2, 'jonas');
+insert into cliente(id, nome) values (3, 'marcos');
+insert into cliente(id, nome) values (4, 'mikasa');
