32ad4f25edad1cb2ce76c250b47a232350d0c8b4
Wildrimak <finalwildrimak@gmail.com>
2021-09-10 15:41:55 -0300

    Iniciado

---
+++ src/test/java/br/com/wildrimak/security/oauth/SecurityOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wildrimak.security.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
